8f29d0ac526d67a827779aad9b8297538224ed72
Ziga <ziga.zajc007@gmail.com>
2023-01-12 23:17:58 +0100

    Update

--- website/functions/creator/[[username]].js
+++ website/functions/creator/[[username]].js
@@ -2,10 +2,6 @@ let paths;
 let username;
 let html = null;
 
-async function serveFile(id, type){
-
-}
-
 export async function onRequest(context) {
   paths = context.params.username;
 
@@ -18,8 +14,15 @@ export async function onRequest(context) {
 
   // User Main Page
   if(paths.length == 1){
+    let key = "content-" + username;
+    // Pull from Cache
+    let cacheKey = context.request.url + "?key=" + key;
+    let res = await cache.match(cacheKey);
+    if(res) html = await res.text();
+    if(html != null) return new Response(html, { headers: { 'content-type': 'text/html;charset=UTF-8' }});
+    // Pull from KV
     html = await context.env.KV.get("content-" + username);
-    if(html == null) return Response.redirect(context.env.DOMAIN, 301);
+    if(html === null) return Response.redirect(context.env.DOMAIN, 301);
     return new Response(html, { headers: { 'content-type': 'text/html;charset=UTF-8' }});
   }
 
