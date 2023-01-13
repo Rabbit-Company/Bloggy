5d43c6693ff46e316aa183afdc46d40432e3e735
Ziga <ziga.zajc007@gmail.com>
2023-01-13 09:06:23 +0100

    Update

---
+++ website/functions/[file].js
@@ -0,0 +1,36 @@
+let paths;
+let username;
+let request;
+let env;
+const cache = caches.default;
+
+async function getValue(key, cacheTime = 600){
+	let value = null;
+
+	let cacheKey = request.url + "?key=" + key;
+	let res = await cache.match(cacheKey);
+	if(res) value = await res.text();
+
+	if(value == null){
+		value = await env.KV.get(key, { cacheTtl: cacheTime });
+		let nres = new Response(value);
+		nres.headers.append('Cache-Control', 's-maxage=' + cacheTime);
+		if(value != null) await cache.put(cacheKey, nres);
+	}
+
+	return value;
+}
+
+async function getFile(key, type = 'text/html;charset=UTF-8', redirect = env.DOMAIN){
+  let value = await getValue(key);
+  if(value !== null) return new Response(value, { headers: { 'content-type': type }});
+  return Response.redirect(redirect, 307);
+}
+
+export async function onRequest(context) {
+  request = context.request;
+  env = context.env;
+  paths = context.params.file;
+
+  return new Response(JSON.stringify(paths));
+}
